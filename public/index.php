8eca794cb3b21c1b2a241cd5aaa135022c34859a
oroshnivskyy <oroshnivskyy@cogniance.com>
2013-01-20 18:16:54 +0200

    Added routing, and moved routes to app

--- public/index.php
+++ public/index.php
@@ -4,24 +4,27 @@ define( 'BASE_PATH', __DIR__ . '/..' );
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing;
 
 $request = Request::createFromGlobals();
-$response = new Response();
+$routes = include __DIR__ . '/../app/app.php';
 
-$routes = array(
-    '/hello' => 'hello',
-    '/bye' => 'bye',
-);
+$context = new Routing\RequestContext();
+$context->fromRequest( $request );
+$matcher = new Routing\Matcher\UrlMatcher( $routes, $context );
 
-$path = $request->getPathInfo();
+try{
+    // Extracts _route and other routing parameters
+    extract( $matcher->match( $request->getPathInfo() ), EXTR_SKIP );
 
-if ( isset( $routes[ $path ] ) ){
     ob_start();
-    extract( $request->query->all(), EXTR_SKIP );
-    include sprintf( BASE_PATH . '/src/pages/%s.php', $routes[ $path ] );
+    include sprintf( BASE_PATH . '/src/pages/%s.php', $_route );
+
     $response = new Response( ob_get_clean() );
-} else{
+} catch ( Routing\Exception\ResourceNotFoundException $e ){
     $response = new Response( 'Not Found', 404 );
+} catch ( Exception $e ){
+    $response = new Response( 'An error occurred', 500 );
 }
 
 $response->send();
