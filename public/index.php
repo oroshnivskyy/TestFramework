b216b92e214b8f44e052460006343f68e2f6809f
oroshnivskyy <oroshnivskyy@cogniance.com>
2013-01-20 13:49:00 +0200

    Replaced Symfony class loader with composer class loader

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../app/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,4 +10,12 @@ $request = Request::createFromGlobals();
 $input = $request->get( 'name', 'World' );
 
 $response = new Response( 'Hello ' . htmlspecialchars( $input, ENT_QUOTES, 'UTF-8' ) );
+
+// Ensure that our Response were compliant with the HTTP specification
+$response->prepare( $request );
+
+$response->setStatusCode(200);
+$response->headers->set('Content-Type', 'text/html');
+$response->setMaxAge( 10 );
+
 $response->send();
