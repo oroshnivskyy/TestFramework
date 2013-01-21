0ede6c1746b8cbf42b9a6952e9c49e74484c225d
oroshnivskyy <oroshnivskyy@cogniance.com>
2013-01-22 00:21:26 +0200

    Added Test Cases

--- public/index.php
+++ public/index.php
@@ -11,6 +11,7 @@ $request = Request::createFromGlobals();
 $routes = include BASE_PATH . '/app/app.php';
 
 $context = new Routing\RequestContext();
+$context->fromRequest( $request );
 $matcher = new Routing\Matcher\UrlMatcher( $routes, $context );
 $resolver = new HttpKernel\Controller\ControllerResolver();
 
