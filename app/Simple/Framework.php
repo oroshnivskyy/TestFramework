5cd67fc06c4e21380495692aeaf4a0147f73b35a
oroshnivskyy <oroshnivskyy@cogniance.com>
2013-02-08 17:44:07 +0200

    Moved code to Framevork

--- app/Simple/Framework.php
+++ app/Simple/Framework.php
@@ -1,14 +1,22 @@
 <?php
 namespace Simple;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
-use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
+use Symfony\Component\Routing;
+use Symfony\Component\HttpKernel;
 use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Component\HttpKernel\HttpKernel;
 
-class Framework extends HttpKernel{
+class Framework extends HttpKernel\HttpKernel{
+    public function __construct( $routes ){
+        $context = new Routing\RequestContext();
+        $matcher = new Routing\Matcher\UrlMatcher( $routes, $context );
+        $resolver = new HttpKernel\Controller\ControllerResolver();
 
+        $dispatcher = new EventDispatcher();
+        $dispatcher->addSubscriber( new HttpKernel\EventListener\RouterListener( $matcher ) );
+        $dispatcher->addSubscriber( new HttpKernel\EventListener\ResponseListener( 'UTF-8' ) );
+        $listener = new HttpKernel\EventListener\ExceptionListener( "Simple\ErrorController::exceptionAction" );
+        $dispatcher->addSubscriber( $listener );
+
+        parent::__construct( $dispatcher, $resolver );
+    }
 }
--- public/index.php
+++ public/index.php
@@ -2,35 +2,16 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 define( 'BASE_PATH', __DIR__ . '/..' );
 
+require_once BASE_PATH . '/vendor/autoload.php';
+
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing;
-use Symfony\Component\HttpKernel;
-use Symfony\Component\EventDispatcher\EventDispatcher;
 
 $request = Request::createFromGlobals();
 $routes = include BASE_PATH . '/app/app.php';
 
-$context = new Routing\RequestContext();
-$matcher = new Routing\Matcher\UrlMatcher( $routes, $context );
-$resolver = new HttpKernel\Controller\ControllerResolver();
+$framework = new Simple\Framework( $routes );
 
-$dispatcher = new EventDispatcher();
-$dispatcher->addSubscriber( new Simple\ResponseSubscriber() );
-$dispatcher->addSubscriber ( new HttpKernel\EventListener\RouterListener( $matcher ) );
-$listener = new HttpKernel\EventListener\ExceptionListener( "Simple\ErrorController::exceptionAction" );
-$dispatcher->addSubscriber( $listener );
-$dispatcher->addSubscriber( new HttpKernel\EventListener\ResponseListener( 'UTF-8' ) );
-$dispatcher->addSubscriber( new Simple\StringResponseListener() );
-
-// For StreamedResponses
-//$dispatcher->addSubscriber(new HttpKernel\EventListener\StreamedResponseListener());
-
-$framework = new Simple\Framework( $dispatcher, $resolver );
-//$framework = new HttpKernel\HttpCache\HttpCache( $framework, new HttpKernel\HttpCache\Store( BASE_PATH . '/cache' ) );
-$response = $framework->handle( $request );
-
-$response->send();
+$framework->handle( $request )->send();
 
 function render_template( \Symfony\Component\HttpFoundation\Request $request ){
     // Extracts _route and other routing parameters
